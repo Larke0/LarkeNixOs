63cd94f8aa29600c5486add1e15d6ee402716146
Larke0 <larke850@gmail.com>
2026-07-12 16:38:24 -0300

    Fixed polkit permission for restart and poweroff

--- configuration.nix
+++ configuration.nix
@@ -443,6 +443,21 @@
   ###################
   ### NIX         ###
   ###################
+  security.polkit.extraConfig = ''
+    polkit.addRule(function(action, subject) {
+      if (
+        subject.isInGroup("users") && (
+          action.id == "org.freedesktop.login1.reboot" ||
+          action.id == "org.freedesktop.login1.reboot-multiple-sessions" ||
+          action.id == "org.freedesktop.login1.power-off" ||
+          action.id == "org.freedesktop.login1.power-off-multiple-sessions"
+        )
+      ) {
+        return polkit.Result.YES;
+      }
+    });
+  '';  
+
   
   environment.extraInit = ''
     # Fixes OBS plugins missing on native Wayland/Hyprland sessions
